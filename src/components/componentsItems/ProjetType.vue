009134522e0d8acd820eb67729db87030a325ead
julien <julienchauvin@hotmail.fr>
2024-08-26 23:31:57 +0200

    fin de la structure projetType avec ajout de la speudo class ainsi que du responsive

--- src/components/componentsItems/ProjetType.vue
+++ src/components/componentsItems/ProjetType.vue
@@ -6,31 +6,64 @@
   description: {
     type: String
   },
-  date: {
+  techno: {
     type: String
   }
 })
 </script>
 
 <template>
-    <div>
-        <h3>
+    <div class="projet__type">
+        <h3 class="titre3">
             {{ title }}
         </h3>
         <span>
             {{ description }}
         </span>
-        <slot>
-        </slot>
+        <br/>
         <span>
-            {{ date }}
+            {{ techno }}
         </span>
+        <br/>
+        <slot>
+
+        </slot>
 
     </div>
 </template>
 
 <style scoped>
+    .projet__type{
+        width: 33%;
+        display: flex;
+        flex-direction: column;
+        padding: 20px;
+        margin: 10px;
+        box-sizing: border-box;
+        border: solid 1px black;
+        
+    }
+
+    .titre3{
+        font-family: 'Robot';
+        text-decoration: underline;
+
+
+    }
     span{
         display: block;
+        font-family: 'Jura';
+        font-weight: 600;
     }
+
+    .projet__type:hover{
+        box-shadow:12px 12px 12px rgba(0, 0, 0, 0.5);
+    }
+    
+    @media screen and (max-width: 650px){
+        .projet__type{
+          width: 100%;
+        }
+    }
+        
 </style>
